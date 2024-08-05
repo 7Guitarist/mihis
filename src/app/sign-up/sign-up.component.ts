5a7fce4c61445122ec148a922596f6ca87575f43
johndoe <johndoe@gmail.com>
2024-08-05 09:26:54 +0800

    passwords

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterLink } from '@angular/router';
 import {
+  AbstractControl,
+  FormBuilder,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
@@ -10,6 +12,8 @@ import {
 } from '@angular/forms';
 import { UsernameValidators } from '../validators/username.validators';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { PasswordValidators } from '../validators/password.validators';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sign-up',
@@ -25,33 +29,76 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-  signUpForm = new FormGroup({
-    firstName: new FormControl(
-      '',
-      [
-        Validators.required,
-        Validators.minLength(2),
-        UsernameValidators.cannotContainSpace,
-      ],
-      UsernameValidators.shouldBeUnique
-    ),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
-  });
+  signUpForm;
+  // signUpForm = new FormGroup(
+  //   {
+  //     firstName: new FormControl('', [
+  //       Validators.required,
+  //       Validators.minLength(2),
+  //     ]),
+  //     lastName: new FormControl('', [
+  //       Validators.required,
+  //       Validators.minLength(2),
+  //     ]),
+  //     password: new FormControl('', [
+  //       Validators.required,
+  //       Validators.minLength(6),
+  //     ]),
+  //     confirmPassword: new FormControl('', [Validators.required]),
+  //   },
+  //   {
+  //     validators: PasswordValidators.passwordShouldMatch,
+  //   }
+  // );
+
+  constructor(fb: FormBuilder) {
+    this.signUpForm = fb.group(
+      {
+        firstName: ['', [Validators.required, Validators.minLength(2)]],
+        lastName: ['', [Validators.required, Validators.minLength(2)]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validators: PasswordValidators.passwordShouldMatch,
+      }
+    );
+  }
 
   get firstName() {
     return this.signUpForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.signUpForm.get('lastName');
   }
 
   get password() {
     return this.signUpForm.get('password');
   }
 
-  singUp() {
-    this.signUpForm.setErrors({
-      invalidLogin: true,
-    });
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
+  }
+
+  confirmPasswordValid: boolean = false;
+  passwordMismatchError: boolean = false;
+
+  // ngOnInit is the lifecycle hook where we set up the form value changes subscription
+  ngOnInit(): void {
+    // Subscribe to form value changes
+    this.signUpForm.valueChanges
+      .pipe(
+        // Delay the validation check by 700 milliseconds
+        debounceTime(700),
+        // Only proceed if the value has actually changed
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        // Update the boolean flags based on the form's current validity state
+        this.confirmPasswordValid = this.confirmPassword?.valid || false;
+        this.passwordMismatchError =
+          this.signUpForm.errors?.['passwordShouldMatch'] || false;
+      });
   }
 }
